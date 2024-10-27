b22433a56f1ed98a55bdb297a7ca9f505dd83f89
Garrett <garrett.mer9@gmail.com>
2024-10-27 12:02:58 +0100

    view for user

--- auth_service/api/users/views.py
+++ auth_service/api/users/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import status, permissions
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from users.serializers import CustomUserSerializer
+
+
+class CustomUserView(APIView):
+    serializer_class = CustomUserSerializer
+    queryset = get_user_model().objects.all()
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, pk=None):
+        if pk:
+            user = get_object_or_404(self.queryset, pk=pk)
+            serializer = self.serializer_class(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        else:
+            users = self.queryset.all()
+            serializer = self.serializer_class(users, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def put(self, request, pk=None):
+        if pk is None:
+            return Response({"detail": "Method 'PUT' not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        user = get_object_or_404(self.queryset, pk=pk)
+        serializer = self.serializer_class(user, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, pk=None):
+        if pk is None:
+            return Response({"detail": "Method 'DELETE' not allowed"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        user = get_object_or_404(self.queryset, pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
