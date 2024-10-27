397eef21a70b976836a8ce0d61c9a5dbb160ef13
Garrett <garrett.mer9@gmail.com>
2024-10-27 12:04:34 +0100

    register user in admin

--- auth_service/api/users/admin.py
+++ auth_service/api/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users import models
+
+
+class CustomUserAdmin(UserAdmin):
+    model = get_user_model()
+
+
+admin.site.register(models.CustomUser, CustomUserAdmin)
