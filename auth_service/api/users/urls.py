169154d35644112372f566c18c1fb11acd68528f
Garrett <garrett.mer9@gmail.com>
2024-12-04 18:11:50 +0100

    routers for user api

--- auth_service/api/users/urls.py
+++ auth_service/api/users/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from users.views import CustomUserView
+
 app_name = "users"
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("users/", CustomUserView.as_view(), name="user-list-create"),
+    path('users/<uuid:pk>/', CustomUserView.as_view(), name='get_user'),
 ]
