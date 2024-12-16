b94ebc8dad360bcadb548461cc8091d150dc6e9f
Garrett <garrett.mer9@gmail.com>
2024-12-16 08:23:23 +0100

    add and set product app

--- product_service/api/config/settings.py
+++ product_service/api/config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import products
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third part
+    'rest_framework',
+    # apps
+    'products.apps.ProductsConfig',
+
 ]
 
 MIDDLEWARE = [
