3fc55800ec9e7de28b82bae7bd32a08e5ee85d7a
Garrett <garrett.mer9@gmail.com>
2024-12-17 08:12:09 +0100

    categories and subcategories

--- product_service/api/products/models.py
+++ product_service/api/products/models.py
@@ -7,7 +7,7 @@ from products.utils import OwnerModel, TimeStampModel
 class Product(TimeStampModel, OwnerModel):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     stock_count = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products/%Y/%m/%d')
     slug = models.SlugField(blank=True, max_length=255, unique=True)
@@ -24,3 +24,32 @@ class Product(TimeStampModel, OwnerModel):
             self.slug = slugify(self.name)
         self.full_clean()
         super().save(*args, **kwargs)
+
+
+class CategoryProduct(models.Model):
+    product_id = models.ForeignKey("Product", on_delete=models.CASCADE)
+    category_id = models.ForeignKey("Category", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    products = models.ManyToManyField("Product", related_name="categories", through="CategoryProduct")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, related_name="subcategories")
+    products = models.ManyToManyField("Product", related_name="subcategories")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Subcategories"
