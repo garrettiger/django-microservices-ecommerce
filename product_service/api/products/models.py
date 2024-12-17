2c7eee0bb24d9bb8f522e5a64498c4b372039b48
Garrett <garrett.mer9@gmail.com>
2024-12-17 07:56:23 +0100

    product model

--- product_service/api/products/models.py
+++ product_service/api/products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from products.utils import OwnerModel, TimeStampModel
+
+
+class Product(TimeStampModel, OwnerModel):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    stock_count = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products/%Y/%m/%d')
+    slug = models.SlugField(blank=True, max_length=255, unique=True)
+    popularity = models.PositiveIntegerField(default=0, help_text="Incrementation when user sees details page.")
+    sales_count = models.PositiveIntegerField(default=0)
+    barcode = models.CharField(max_length=13, blank=True)
+    rank = models.IntegerField(help_text="Rank from review service")
