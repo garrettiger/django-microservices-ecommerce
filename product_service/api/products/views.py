d3d66e2bb8cb78637f064be188bae4e2e9a6df45
Garrett <garrett.mer9@gmail.com>
2024-12-18 07:14:36 +0100

    views for category

--- product_service/api/products/views.py
+++ product_service/api/products/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from products.models import Category
+from products.paginators import CustomPaginator
+from products.serialziers import CategorySerializer, CategoryWithSubcategoriesSerializer
+
+
+class CategoryListView(ListAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+    pagination_class = CustomPaginator
+    permission_classes = (AllowAny,)
+
+
+class CategoryRetrieveView(RetrieveAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+    permission_classes = (AllowAny,)
+
+
+class CategoryWithSubcategoriesRetrieveView(RetrieveAPIView):
+    serializer_class = CategoryWithSubcategoriesSerializer
+    queryset = Category.objects.all()
+    permission_classes = (AllowAny,)
