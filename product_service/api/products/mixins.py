e59727bcfbfd72d6882eebbf01c9f19b4adcf04b
Garrett <garrett.mer9@gmail.com>
2024-12-18 07:39:44 +0100

    cahce mixin

---
+++ product_service/api/products/mixins.py
@@ -0,0 +1,12 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+
+
+class CacheMixin:
+    cache_timeout = 60 * 15
+
+    def get_cache_timeout(self):
+        return self.cache_timeout
+
+    def dispatch(self, *args, **kwargs):
+        return method_decorator(cache_page(self.get_cache_timeout()))()
