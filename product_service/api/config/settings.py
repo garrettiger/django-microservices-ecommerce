04c79690009a54a0500b76ad37cc15fd8de833c4
Garrett <garrett.mer9@gmail.com>
2024-12-30 07:52:30 +0100

    delete auth user field

--- product_service/api/config/settings.py
+++ product_service/api/config/settings.py
@@ -144,8 +144,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-AUTH_USER_MODEL = "users.CustomUser"
-
 IMAGES_TYPES = ["jpg", "png", "jpeg"]
 
 MAX_UPLOAD_SIZE = 512000  # 500kb
