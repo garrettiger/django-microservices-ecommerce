ee8540f174c6bef778adde452c7a5645ac25bbf4
Garrett <garrett.mer9@gmail.com>
2025-01-04 08:02:16 +0100

    add superuser migration, add logs folder

---
+++ auth_service/api/users/migrations/0002_superuser.py
@@ -0,0 +1,36 @@
+import os
+
+from django.db import migrations
+
+
+def create_superuser(apps, schema_editor):
+    User = apps.get_model("users", "CustomUser")
+
+    DJ_SU_NAME = os.environ.get("DJ_SU_NAME")
+    DJ_SU_PASSWORD = os.environ.get("DJ_SU_PASSWORD")
+    DJ_SU_EMAIL = os.environ.get("DJ_SU_EMAIL")
+
+    User.objects.create_superuser(
+        username=DJ_SU_NAME,
+        email=DJ_SU_EMAIL,
+        password=DJ_SU_PASSWORD,
+    )
+
+
+def delete_superuser(apps, schema_editor):
+    User = apps.get_model("users", "CustomUser")
+    admin = User.objects.get(pk=1)
+    if admin.is_superuser:
+        admin.delete()
+    else:
+        raise IndexError("User with id = 1 is not an admin")
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("users", "0001_initial"),
+    ]
+    operations = [
+        migrations.RunPython(create_superuser, delete_superuser),
+    ]
