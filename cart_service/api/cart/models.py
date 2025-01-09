3f300de889929401e60944030b124cafc851d22d
Garrett <garrett.mer9@gmail.com>
2025-01-09 18:28:16 +0100

    cart and cart item models

--- cart_service/api/cart/models.py
+++ cart_service/api/cart/models.py
@@ -1,3 +1,15 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from api.cart.utils import TimeStampModel, OwnerModel
+
+
+class Cart(TimeStampModel, OwnerModel):
+    cart_id = models.UUIDField(default=uuid.uuid4, editable=False)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product_id = models.IntegerField()
+    quantity = models.IntegerField()
