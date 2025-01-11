a3c0585ca8d70f4d71681ca7d8dff8608ad795dc
Garrett <garrett.mer9@gmail.com>
2025-01-11 07:27:50 +0100

    logging and logging config

--- review_service/api/config/settings.py
+++ review_service/api/config/settings.py
@@ -10,9 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import logging.config
 from pathlib import Path
 
 import dj_database_url
+
+from config import settings_logging
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Logging
+LOGGING_CONFIG = None
+logging.config.dictConfig(settings_logging.get_logger_config(BASE_DIR))
