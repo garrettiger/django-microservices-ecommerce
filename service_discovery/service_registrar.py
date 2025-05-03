515a483a0a7b026ce90efe84010228deee0107bd
Garrett <garrett.mer9@gmail.com>
2025-05-03 17:46:26 +0200

    change service registrant, add tags for traefic

--- service_discovery/service_registrar.py
+++ service_discovery/service_registrar.py
@@ -26,11 +26,21 @@ def get_running_services():
 
 
 def register_service(service_name, service_id, address, port):
+    middleware_name = f"{service_name}-strip"
+    tags = [
+        "traefik.enable=true",
+        f"traefik.http.routers.{service_name}.rule=PathPrefix(`/{service_name}`)",
+        f"traefik.http.routers.{service_name}.entrypoints=web",
+        f"traefik.http.routers.{service_name}.middlewares={middleware_name}",
+        f"traefik.http.middlewares.{middleware_name}.stripprefix.prefixes=/{service_name}",
+        f"traefik.http.services.{service_name}.loadbalancer.server.port={port}"
+    ]
     payload = {
         "Name": service_name,
         "ID": service_id,
         "Address": address,
-        "Port": port
+        "Port": port,
+        "Tags": tags,
     }
     try:
         response = requests.put(CONSUL_URL, json=payload)
